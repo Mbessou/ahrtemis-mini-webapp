32267f8a2a82c901043e4c12c0cc73f4d6cbd15d
MacPro Bessou <maxime.bessou@epitech.eu>
2023-09-28 14:32:27 +0200

    [~] Back: fix contact routes

--- back/controllers/contact.controller.js
+++ back/controllers/contact.controller.js
@@ -1,88 +1,95 @@
 const serviceContact = require('../services/contact.service')
+const httpError = require('http-errors');
 
 exports.getAllContacts = (req, res) => {
     serviceContact.getAll()
         .then((data) => {
             if (!data || !data.length) {
-                res.status(204).send({ contacts: [] });
+                return res.status(204).send({ contacts: [] });
             }
-            res.status(200).send({ contacts: data });
+            return res.status(200).send({ contacts: data });
         })
         .catch((err) => {
-            res.status(500).send(httpError(500, { error_description: "An error occurred while getting all user" }));
+            console.error('err', err);
+            return res.status(500).send(httpError(500, { error_description: "An error occurred while getting all user" }));
         })
 }
 
 exports.getContactByEmail = (req, res) => {
     if (!req.params.email) {
-        res.status(400).send(httpError(400, { error_description: `Mandatory params missing or wrong (email)` }));
+        return res.status(400).send(httpError(400, { error_description: `Mandatory params missing or wrong (email)` }));
     }
     serviceContact.getOneByEmail(req.params.email)
         .then((data) => {
             if (!data || !data.length) {
-                res.status(204).send({ contacts: null });
+                return res.status(204).send({ contacts: null });
             }
-            res.status(200).send({ contact: data });
+            return res.status(200).send({ contact: data });
         })
         .catch((err) => {
-            res.status(500).send(httpError(500, { error_description: "An error occurred while getting user by email" }));
+            console.error('err', err);
+            return res.status(500).send(httpError(500, { error_description: "An error occurred while getting user by email" }));
         })
 }
 
 exports.getContact = (req, res) => {
     if (!req.params.id) {
-        res.status(400).send(`Mandatory params missing (id)`);
+        return res.status(400).send(`Mandatory params missing (id)`);
     }
     serviceContact.getOne(req.params.id)
         .then((data) => {
             if (!data || !data.length) {
-                res.status(204).send({ contacts: null });
+                return res.status(204).send({ contacts: null });
             }
-            res.status(200).send({ contact: data });
+            return res.status(200).send({ contact: data });
         })
         .catch((err) => {
-            res.status(500).send(httpError(500, { error_description: "An error occurred while getting user" }));
+            console.error('err', err);
+            return res.status(500).send(httpError(500, { error_description: "An error occurred while getting user" }));
         })
 }
 
 exports.createContact = (req, res) => {
     if (!req.body || !req.body.email) {
-        res.status(400).send(httpError(400, { error_description: `Mandatory body fields missing (email)` }));
+        return res.status(400).send(httpError(400, { error_description: `Mandatory body fields missing (email)` }));
 
     }
     serviceContact.add(req.body)
         .then((data) => {
-            res.status(201).send(`User ${id} created successfully`);
+            return res.status(201).send(`User created successfully`);
         })
         .catch((err) => {
-            res.status(500).send(httpError(500, { error_description: "An error occurred while creating user" }));
+            console.error('err', err);
+            return res.status(500).send(httpError(500, { error_description: "An error occurred while creating user" }));
         })
 }
 
 exports.updateContact = (req, res) => {
     if (!req.body || !req.body.email) {
-        res.status(400).send(httpError(400, { error_description: `Mandatory body fields missing (email)` }));
+        return res.status(400).send(httpError(400, { error_description: `Mandatory body fields missing (email)` }));
     } else if (!req.params.id) {
-        res.status(400).send(httpError(400, { error_description: `Mandatory params missing (id)` }));
+        return res.status(400).send(httpError(400, { error_description: `Mandatory params missing (id)` }));
     }
     serviceContact.updateById(req.params.id, req.body)
         .then((data) => {
-            res.status(200).send(`User ${id} updated successfully`);
+            return res.status(200).send(`User ${req.params.id} updated successfully`);
         })
         .catch((err) => {
-            res.status(500).send(httpError(500, { error_description: "An error occurred while updating user" }));
+            console.error('err', err);
+            return res.status(500).send(httpError(500, { error_description: "An error occurred while updating user" }));
         })
 }
 
 exports.deleteContact = (req, res) => {
     if (!req.params.id) {
-        res.status(400).send(httpError(400, { error_description: `Mandatory params missing (id)` }));
+        return res.status(400).send(httpError(400, { error_description: `Mandatory params missing (id)` }));
     }
     serviceContact.deleteById(req.params.id)
         .then((data) => {
-            res.status(200).send(`User ${id} deleted successfully`);
+            return res.status(200).send(`User ${req.params.id} deleted successfully`);
         })
         .catch((err) => {
-            res.status(500).send(httpError(500, { error_description: "An error occurred while deleting user" }));
+            console.error('err', err);
+            return res.status(500).send(httpError(500, { error_description: "An error occurred while deleting user" }));
         })
 }
--- back/services/contact.service.js
+++ back/services/contact.service.js
@@ -1,21 +1,21 @@
 const pgp = require('pg-promise')()
-const db = pgp(`postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DATABASE}`)
+const db = pgp(`postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`)
 
 exports.getAll = async () => {
-    return await db.one('SELECT * FROM contacts')
+    return await db.manyOrNone('SELECT * FROM contacts')
 }
 
 exports.getOne = async (id) => {
-    return await db.one('SELECT * FROM contacts WHERE id = $1', id)
+    return await db.oneOrNone('SELECT * FROM contacts WHERE id = $1', id)
 }
 
 exports.getOneByEmail = async (email) => {
-    return await db.one('SELECT * FROM contacts WHERE email = $1', id)
+    return await db.oneOrNone('SELECT * FROM contacts WHERE email = $1', id)
 }
 
 exports.updateById = async (id, body) => {
-    const { firstname, lastname, email, address, phoneNumber, age } = body;
-    const values = [firstname, lastname, email, address, phoneNumber, age, id]
+    const { firstname, lastname, email, address, phonenumber, age } = body;
+    const values = [firstname, lastname, email, address, phonenumber, age, id]
     const query = `
         UPDATE contacts
         SET
@@ -23,23 +23,23 @@ exports.updateById = async (id, body) => {
             lastname = $2,
             email = $3,
             address = $4,
-            phoneNumber = $5,
+            phonenumber = $5,
             age = $6
         WHERE id = $7
     `
-    return await db.one(query, values)
+    return await db.oneOrNone(query, values)
 }
 
 exports.add = async (body) => {
-    const { firstname, lastname, email, address, phoneNumber, age } = body;
-    const values = [firstname, lastname, email, address, phoneNumber, age]
+    const { firstname, lastname, email, address, phonenumber, age } = body;
+    const values = [firstname, lastname, email, address, phonenumber, age]
     const query = `
-        INSERT INTO contacts (firstname, lastname, email, address, phoneNumber, age)
+            INSERT INTO contacts (firstname, lastname, email, address, phonenumber, age)
         VALUES ($1, $2, $3, $4, $5, $6)
     `
-    return await db.one(query, values)
+    return await db.oneOrNone(query, values)
 }
 
 exports.deleteById = async (id) => {
-    return await db.one('DELETE FROM contacts WHERE id = $1', id)
+    return await db.oneOrNone('DELETE FROM contacts WHERE id = $1', id)
 }
