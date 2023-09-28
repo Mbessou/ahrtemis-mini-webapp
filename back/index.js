ed29e68fd5c559a587a70b9516d24add6d74574a
MacPro Bessou <maxime.bessou@epitech.eu>
2023-09-28 14:31:43 +0200

    [+] Back: Add bodyParser + cors package

--- back/index.js
+++ back/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+const cors = require('cors');
 const port = process.env.BACKEND_PORT;
 const router = require('./routes/contact.routes');
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+app.use(cors());
 
 app.use('/', router);
 
 app.listen(port || 4000, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
